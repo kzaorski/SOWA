f298c14c00672598305eb5a55e663927c84e463a
Krzysiek <zaorski@gmail.com>
2017-11-29 14:47:35 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( 'onwheel' in document ) {
+		window.onwheel = function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'DOMMouseScroll', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	} else if ( 'onmousewheel' in document ) {
+		window.onmousewheel= function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'onmousewheel', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	}
+
+	try { $('body').unmousewheel(); } catch( error ) {}
+});
+
 function include(scriptUrl) {
     document.write('<script src="' + scriptUrl + '"></script>');
 }
